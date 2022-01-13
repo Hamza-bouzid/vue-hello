03a4a81f7fd539e9ae01b363c264e0ba90d642e1
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-01-13 13:09:23 +0100

    completato

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+    image: "https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg",
+  },
+});
