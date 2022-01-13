b5142c9f11536a126441849fa930d31d5839cfb7
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-01-13 13:24:16 +0100

    completato + bonus

--- main.js
+++ main.js
@@ -4,4 +4,14 @@ var app = new Vue({
     message: "Hello Vue!",
     image: "https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg",
   },
+
+  methods: {
+    addImage: function () {
+      this.image = `https://unsplash.it/800/800?image=${randomNumber(1, 100)}`;
+    },
+  },
 });
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
